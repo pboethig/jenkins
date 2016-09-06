3f1df14487b7a93bd137845c6fc1b88c3a5109ba
pboethig <pboethig@gmail.com>
2016-09-07 01:17:20 +0200

    add dockerfiles

---
+++ startup.sh
@@ -0,0 +1,8 @@
+clear
+
+docker-compose up --build -d --force-recreate
+
+echo 'starting jenkins service in container jenkins_jenkins_1'
+docker exec -it jenkins_jenkins_1 /etc/init.d/jenkins restart
+
+echo 'you can surf http://localhost:8080 now'
