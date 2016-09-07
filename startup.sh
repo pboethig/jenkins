00a39b6ea523a0647aaa356160f486a64a3becdb
pboethig <pboethig@gmail.com>
2016-09-07 06:15:47 +0200

    add windows support and change the defaultport to 8081

--- startup.sh
+++ startup.sh
@@ -7,4 +7,6 @@ docker-compose up --build -d --force-recreate
 echo 'starting jenkins service in container jenkins_jenkins_1'
 docker exec -it jenkins_jenkins_1 /etc/init.d/jenkins restart
 
-echo 'you can surf http://localhost:8080 now'
+echo 'you can surf http://localhost:8081 now'
+
+echo 'On Windows and Mac http://<docker-machine ip>:8081'
