6c4a855a4c2f622cc10794a5dc03ae6116f8290d
pboethig <pboethig@gmail.com>
2016-09-10 11:38:53 +0200

    fix startup script

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ docker-compose down
 
 docker-compose up --build -d --force-recreate
 
-echo 'starting jenkins service in container jenkins_jenkins_1'
+echo 'starting jenkins service in container dockerjenkins_jenkins_1'
 docker exec -it jenkins_jenkins_1 /etc/init.d/jenkins restart
 
 echo 'you can surf http://localhost:8081 now'
